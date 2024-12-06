56d131cd7d066b6b0a2b58ae47e7f1798bb73fa6
Josiah <josiah.lugo@vtsd.com>
2024-12-06 12:56:37 -0500

    1-2-4 final product

--- 1-2-4/main.py
+++ 1-2-4/main.py
@@ -1,14 +1,16 @@
 import turtle as trtl
 import random as rand
-#Intialize Variables
+
+# Intialize Variables
 wn = trtl.Screen()
 maze_painter = trtl.Turtle()
-wall_len = 35
 path_width = 30
+
 # Setup Turtle
 maze_painter.left(90)
 maze_painter.pensize(5)
 maze_painter.speed(0)
+
 # Draw Maze
 # Process:
 # Draw a line
@@ -16,34 +18,26 @@ maze_painter.speed(0)
 # Increment Length
 # Repeat
 def draw_barrier():
-    maze_painter.forward(40)
-    maze_painter.left(90)
-    maze_painter.forward(path_width * 2)
-    maze_painter.back(path_width * 2)
     maze_painter.right(90)
-
-
-def draw_door():
-    maze_painter.forward(10)
-    maze_painter.penup()
-    maze_painter.forward(path_width * 2)
-    maze_painter.pendown()
-
-for wall in range(21):
-    draw_door()
-    if (wall > 5):
-        draw_barrier()
-    maze_painter.forward(wall_len - path_width - (wall_len / 3))
+    maze_painter.forward(path_width)
+    maze_painter.backward(path_width)
     maze_painter.left(90)
-    wall_len += 15
 
-
-'''
-# randomize location of doors and barriers in wall
-door = rand.randint(path_width*2, (wall_len - path_width*2))
-    barrier = rand.randint(path_width*2, (wall_len - path_width*2))
-'''
-
+length = 35
+for wall in range(21):
+    gap_space = rand.randint(0, length - path_width)
+    door_space = 0
+    maze_painter.forward(gap_space)
+    maze_painter.penup()
+    maze_painter.forward(path_width)
+    maze_painter.pendown()
+    if(wall > 5):
+       door_space = rand.randint(0, length - path_width - gap_space)
+       maze_painter.forward(door_space)
+       draw_barrier()
+    maze_painter.forward(length - gap_space - path_width - door_space)
+    maze_painter.left(90)
+    length += 15
 
 wn.listen()
 wn.mainloop()
